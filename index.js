30b44a853c6b22c771796aa28f9d694767997d07
Dan Peddle <dan.peddle@gmail.com>
2015-01-18 18:23:58 +0100

    cleanup

--- index.js
+++ index.js
@@ -4,55 +4,36 @@ var fs = require('fs');
 var jsonFm = require('json-front-matter');
 var yamlFm = require('yaml-front-matter');
 
-function transform(file){
-    
+function transform(file) {
+
     var ext = path.extname(file);
-    var fmType;
+    var parser;
 
     if (ext !== '.fm') {
         return through();
     }
 
     if (/.json.fm$/g.test(file)) {
-        fmType = 'json';
+        parser = jsonFm.parse.bind(jsonFm);
     } else if (/.yaml.fm$/g.test(file)) {
-        fmType = 'yaml';
+        parser = yamlFm.parse.bind(yamlFm);
     }
 
     var buffer = '';
-
-    switch(fmType) {
-        case 'json':
-            
-            return through(function (chunk) {
-                buffer += chunk;
-            },function(){
-                var parsed = jsonFm.parse(buffer);
-                parsed = 'module.exports='+JSON.stringify(parsed);
-                this.queue(parsed);
-                this.queue(null);
-            });
-
-            break;
-        case 'yaml':
-            return through(function (chunk) {
-                buffer += chunk;
-            },function(){
-                var parsed = yamlFm.parse(buffer);
-                parsed = 'module.exports='+JSON.stringify(parsed);
-                this.queue(parsed);
-                this.queue(null);
-            });
-
-            break;
-        default:
-            console.log('unknown frontmatter type:', file);
-            return through();
-            break;
-
+    if (!parser) {
+        console.log('unknown frontmatter type:', file);
+        return through();
     }
 
-    
+    return through(function(chunk) {
+        buffer += chunk;
+    }, function() {
+        var parsed = parser(buffer);
+        parsed = 'module.exports=' + JSON.stringify(parsed) + ';';
+        this.queue(parsed);
+        this.queue(null);
+    });
+
 }
 
 module.exports = transform;
